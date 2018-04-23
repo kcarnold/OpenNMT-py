905ecbf064c8dce8da845126df7151b0b07534c3
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2018-04-23 15:57:27 -0400

    Split out loading code to allow a slightly less hideous monkey-patch to do the same.

--- onmt/modules/VecsEncoder.py
+++ onmt/modules/VecsEncoder.py
@@ -5,7 +5,6 @@ import torch
 from torch.autograd import Variable
 import h5py
 
-override_h5_filename = None
 
 class VecsEncoder(nn.Module):
     """
@@ -15,10 +14,8 @@ class VecsEncoder(nn.Module):
     """
     def __init__(self, rnn_size, h5_filename):
         super(VecsEncoder, self).__init__()
-        self.h5_filename = h5_filename if override_h5_filename is None else override_h5_filename
-        self.h5_file = h5py.File(self.h5_filename, 'r')
-        self.h5_dataset = self.h5_file['image_features']
-        self.num_vecs, self.num_objs, self.feature_dim = self.h5_dataset.shape
+        self.h5_filename = h5_filename
+        self.num_objs, self.feature_dim = self._open_h5_file()
         self.init_hidden = nn.Linear(in_features=self.feature_dim, out_features=rnn_size, bias=True)
         self.init_cell = nn.Linear(in_features=self.feature_dim, out_features=rnn_size, bias=True)
 
@@ -34,22 +31,33 @@ class VecsEncoder(nn.Module):
     def forward(self, input, lengths=None):
         "See :obj:`onmt.modules.EncoderBase.forward()`"
 
-        # Input: batch_size IntTensor of indices.
-        batch_size = input.size(0)
+        vecs = Variable(
+            self.load_h5_data(input),
+            requires_grad=False)
 
-        # Load corresponding vectors.
+        return self.forward_given_vecs(vecs)
+
+    def _open_h5_file(self):
+        self.h5_file = h5py.File(self.h5_filename, 'r')
+        self.h5_dataset = self.h5_file['image_features']
+        num_vecs, num_objs, feature_dim = self.h5_dataset.shape
+        return num_objs, feature_dim
+
+
+    def load_h5_data(self, indices):
+        batch_size = len(indices)
         vecs = np.empty((batch_size, self.num_objs, self.feature_dim))
-        for i, idx in enumerate(input.data):
+        for i, idx in enumerate(indices):
             vecs[i] = self.h5_dataset[idx]
-        # vecs: batch_size x objs x feature_dim
 
-        # 2. Features need to be first, since they're analogous to words.
+        # Features need to be first, since they're analogous to words.
         vecs = vecs.transpose(1, 0, 2)
         # vecs: objs x batch_size x feature_dim
 
-        # To PyTorch!
-        vecs = Variable(torch.FloatTensor(vecs))
+        return torch.FloatTensor(vecs)
 
+    def forward_given_vecs(self, vecs):
+        # vecs: objs x batch_size x feature_dim
         mean_feature = torch.mean(vecs, dim=0)  # batch_size x feature_dim
 
         # Construct the hidden and cell states.
